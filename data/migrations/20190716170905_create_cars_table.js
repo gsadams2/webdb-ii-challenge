55fa75936f373c2f700d73da0da2c5829258cc3d
George Adams <gadams@Georges-Air.local>
2019-07-16 18:04:36 -0500

    finished mvp and stretch

---
+++ data/migrations/20190716170905_create_cars_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+    tbl
+      .string("VIN")
+      .unique()
+      .notNullable();
+    tbl.string("make", 128).notNullable();
+    tbl.string("model", 128).notNullable();
+    tbl.decimal("mileage").notNullable();
+    tbl.string("transmission", 128);
+    tbl.string("title", 128);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
